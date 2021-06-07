5910000d9bb8b74c6b8ae1714fdf37c39a79a6c0
misterice <dstecker@sploindy.de>
2021-06-07 18:24:15 +0200

    xdebug added to dockerfile

--- src/index.php
+++ src/index.php
@@ -1,51 +1,13 @@
 <?php
 
-/* spPortalSystem index.php
- * Created on 19.05.2009 from misterice
- *
- * spPortalSystem was written by Daniel Stecker 2009
- * please visit my website www.sploindy.de
- *
- * This file is part of spPortalSystem.
- * spPortalSystem is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or any later version.
- *
- * spPortalSystem is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- */
-/**
- * \defgroup systemcore
- * \details
- * Systemcore iss your basic class set
- * \mainpage spPortalSystem Main Page
- *
- * <b>s</b>im<b>p</b>el PortalSystem was coding by Daniel Stecker since 2008
- *
- *
- * This manual is divided in the following sections:
- *
- * - \subpage howto_install
- * - \subpage howto_writemodul
- *
- */
-define('SP_PROFILE', false);
+const SP_PROFILE = false;
 
 if (!file_exists('config.inc.php')) {
     header('Location: ' . substr($_SERVER['PHP_SELF'], 0, strpos($_SERVER['PHP_SELF'], "/index.php")) . '/install/step1.php');
 } else {
     require_once 'config.inc.php';
-    if (SP_CORE_DEBUG > SP_LOG_NOTHING) {
-        if (function_exists("xhprof_enable")) {
-            xhprof_enable();
-        }
+    if (SP_CORE_DEBUG > SP_LOG_NOTHING && function_exists("xhprof_enable")) {
+        xhprof_enable();
     }
     require_once SP_CORE_DOC_ROOT . '/includes/CLog.php';
     require_once SP_CORE_DOC_ROOT . '/includes/CTemplate.php';
